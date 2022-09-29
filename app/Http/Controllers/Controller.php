ae178278286a4bee194d30e4b8f2d2f92afb17a9
Mario Dujmovic <mario7dujmovic@gmail.com>
2022-09-28 21:56:17 -0500

    added user controller and abstract controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    private mixed $data;
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+        $this->data = json_decode($this->getRequest()->getContent());
+    }
+
+    /**
+     * @return Request
+     */
+    public function getRequest(): Request
+    {
+        return $this->request;
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function setRequest(Request $request): void
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData(): mixed
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param mixed $data
+     */
+    public function setData(mixed $data): void
+    {
+        $this->data = $data;
+    }
 }
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\UserService;
+use Exception;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    private $userService = null;
+
+    public function create()
+    {
+        try {
+            $data['email'] = $this->getData()->email;
+            $data['password'] = Hash::make($this->getData()->password);
+            $data['first_name'] = $this->getData()->first_name;
+            $data['last_name'] = $this->getData()->last_name;
+            $data['created_at'] = date('Y-m-d H:i:s');
+            $this->getUserService()->createUser($data);
+            $message = "Success";
+            $code = 200;
+        } catch (Exception $e) {
+            //we don't want to give much info here, because we don't want someone mining for existing users or something like that
+            $message = $e->getMessage();
+            $code = 400;
+        }
+        return response()->json($message, $code);
+    }
+
+    public function authenticate()
+    {
+        try {
+            $email = $this->getRequest()->getUser();
+            $user = $this->getUserService()->getUserRepo()->getUserByEmail($email);
+            if (empty($user)) {
+                return response()->json("Something went wrong.", 400);
+            }
+            $password = Hash::check($this->getRequest()->getPassword(), $user->password);
+            if ($password) {
+                //generate API key and return
+                $data['created_at'] = date('Y-m-d H:i:s');
+                $data['expires'] = date('Y-m-d H:i:s', strtotime('+2 hours'));
+                $user_id = $user->id;
+                $data['user_id'] = $user_id;
+                $data['api_key'] = $this->getUserService()->generateApiKey();
+                $this->getUserService()->getUserRepo()->insertUserApiToken($data);
+                $message = ['api_key' => $data['api_key'], 'expires' => $data['expires']];
+                $code = 200;
+            } else {
+                $message = "Unable to authenticate";
+                $code = 401;
+            }
+        } catch (Exception $e) {
+            $message = $e->getMessage();
+            $code = 400;
+        }
+        return response()->json($message, $code);
+    }
+
+    /**
+     * @return UserService
+     */
+    public function getUserService()
+    {
+        if ($this->userService == null) {
+            $this->userService = new UserService();
+        }
+        return $this->userService;
+    }
+
+    /**
+     * @param $userService
+     * @return void
+     */
+    public function setUserService($userService): void
+    {
+        $this->userService = $userService;
+    }
+}
